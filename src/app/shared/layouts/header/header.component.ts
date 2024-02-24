32c1c787f13dec6c7177d463f5a322e19d5ece81
Santosh Singh <santosh007bca@gmail.com>
2024-02-24 18:34:11 +0530

    login logout auth guard done

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,14 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    imports: [CommonModule, RouterLink],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+    logged_in:boolean =false;
+    language:String = 'English';
+    user_role!:any;
+
+    constructor(private router:Router){}
+
+    ngOnInit(): void {
+        
+    }
+
+    ngDoCheck() {
+        this.user_role = sessionStorage.getItem("role");
+        // console.log(this.user_role);
+        const user_session_id = sessionStorage.getItem("user_session_id");
+        if (user_session_id) {
+            this.logged_in = true;
+        }
+    }
+
+    logout() {
+        sessionStorage.removeItem("user_session_id");
+        sessionStorage.removeItem("role");
+        this.router.navigateByUrl('/sing-in');
+        location.reload();
+    }
 }
--- src/app/shared/services/auth-guard.service.ts
+++ src/app/shared/services/auth-guard.service.ts
@@ -1,9 +1,101 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+// Admin before login check
+@Injectable({
+    providedIn: 'root'
+})
+export class AdminAuthGuardLogin implements CanActivate{
+
+    constructor(private router:Router) { }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        let role = sessionStorage.getItem("role");
+        if (role == "admin") {
+            this.router.navigate(["/admin-dashboard"]);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+// Admin after login check
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class AuthGuardService {
+export class AdminAuthGuardService {
 
-  constructor() { }
+    constructor(private router:Router) { }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        let role = sessionStorage.getItem("role");
+        if (role == "admin") {
+            return true;
+        } else {
+            this.router.navigate(['/admin-login']);
+            return false;
+        }
+    }
+}
+
+
+// customer (buyer and seller) before login check
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SellerBuyerAuthGuardLogin implements CanActivate{
+
+    constructor(private router:Router) { }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        let role = sessionStorage.getItem("role");
+        if (role == "seller") {
+            this.router.navigate(['/seller-dashboard']);
+            return false;
+        } else if(role == "buyer"){
+            this.router.navigate(['/buyer-dashboard']);
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+// buyer after login check
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BuyerAuthGuardService {
+
+    constructor(private router:Router) { }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        let role = sessionStorage.getItem("role");
+        if (role == "buyer") {
+            return true;
+        } else {
+            this.router.navigate(['/sign-in']);
+            return false;
+        }
+    }
+}
+
+// seller after login check
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SellerAuthGuardService {
+
+    constructor(private router:Router) { }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        let role = sessionStorage.getItem("role");
+        if (role == "seller") {
+            return true;
+        } else {
+            this.router.navigate(['/sign-in']);
+            return false;
+        }
+    }
 }
