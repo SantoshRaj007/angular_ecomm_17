1c21a68d5eeb7009c7c55c42bd45307b68155317
Santosh Singh <santosh007bca@gmail.com>
2024-03-02 18:39:45 +0530

    product fetch in buyer-dashboard

--- src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
+++ src/app/customer/buyer/buyer-dashboard/buyer-dashboard.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
 @Component({
-  selector: 'app-buyer-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './buyer-dashboard.component.html',
-  styleUrl: './buyer-dashboard.component.css'
+    selector: 'app-buyer-dashboard',
+    standalone: true,
+    imports: [CommonModule, RouterLink],
+    templateUrl: './buyer-dashboard.component.html',
+    styleUrl: './buyer-dashboard.component.css'
 })
-export class BuyerDashboardComponent {
+export class BuyerDashboardComponent implements OnInit{
+
+    all_products:any;
+    show_Checkout:boolean = false;
+
+    constructor(private router:Router, private customerService:CustomerService) { }
+
+    ngOnInit(): void {
+       this.getAllProduct(); 
+    }
+
+    getAllProduct(){
+        this.customerService.allProduct().subscribe(data=>{
+            this.all_products = data;
+        }, error=>{
+            console.log("My error", error)
+        })
+    }
+
+    buyProduct(id:number){
+        this.show_Checkout = true;
+        this.customerService.quickBuyProduct(id);
+        this.router.navigateByUrl('/checkout');
+    }
+
+    addTOCart(){
+        alert("This is showcase");
+    }
 
 }
